3207df02d3a61450544ad8a9e126a885510e85c2
Pavel Mokshin <mokshinpv@gmail.com>
2024-07-11 19:40:35 +0700

    Initial commit

---
+++ Range/src/ru/academits/java/mokshin/range/Range.java
@@ -0,0 +1,4 @@
+package ru.academits.java.mokshin.range;
+
+public class Range {
+}
---
+++ Range/src/ru/academits/java/mokshin/range_main/Main.java
@@ -0,0 +1,7 @@
+package ru.academits.java.mokshin.range_main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("OK");
+    }
+}
