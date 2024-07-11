51e7e0ee773101a3652767d8f82fc860967ff86d
mokshinpv <mokshinpv@gmail.com>
2024-07-11 19:46:10 +0700

    Update Main.java

--- Range/src/ru/academits/java/mokshin/range_main/Main.java
+++ Range/src/ru/academits/java/mokshin/range_main/Main.java
@@ -3,6 +3,6 @@ package ru.academits.java.mokshin.range_main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("OK");
-        System.out.println(123);
+        System.out.println(1234);
     }
 }
